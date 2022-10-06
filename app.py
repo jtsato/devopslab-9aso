c499d53555cec855434058742a2b2d4279141fd1
jtsato <jorge.takeshi.sato@gmail.com>
2022-10-05 22:17:06 -0300

    feat(sonar): improve sonar scanner

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
-csrf = CSRFProtect()
 app = Flask(__name__)
-csrf.init_app(app)
+
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "There's a point where we just let the music take over everything."
+    return "Laboratório Pipeline DevOps"
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 
 if __name__ == '__main__':
     app.run()
