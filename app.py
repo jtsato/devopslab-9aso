32ea16e6e735d57e44abf1cc66afcb1b3834da88
jtsato <jorge.takeshi.sato@gmail.com>
2022-10-05 21:52:57 -0300

    fix(app): missing import

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
 csrf = CSRFProtect()
-csrf.init_app(app)
+
+def create_app():
+    app = Flask(__name__)
+    csrf.init_app(app)
 
 @app.route("/")
 def pagina_inicial():
